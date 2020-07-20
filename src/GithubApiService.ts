21b0f9cae5636aec77af19484924946b577af74d
tangtang <1987567948@qq.com>
2020-07-20 18:10:04 +0800

    选取部分属性进行打印输出

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -1,15 +1,19 @@
 import * as request from 'request';
+import { User } from './User';
 const options = {
     headers: {
         'User-Agent': 'request'
-    }
+    },
+    json:true
 };
 export class GithubApiService{
     //https://api.github.com/users/fireWinter
     getUserInfo(useName:string){
         request
-        .get('https://api.github.com/users/'+ useName,options,(error:any,response:any,body:any) =>{
-            console.log(body)
+        .get('https://api.github.com/users/'+ useName, options,(error:any,response:any,body:any) => {
+            console.log(body);
+            let user:User = new User(body);
+            console.log(user);
         })
     }
 }
